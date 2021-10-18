b85da5385e5ed5b29d0cabddfe1711f1c8a0953c
5yanfei <92701533+5yanfei@users.noreply.github.com>
2021-10-18 15:01:12 +0800

    Update start_mediaUnlockTest.sh

--- start_mediaUnlockTest.sh
+++ start_mediaUnlockTest.sh
@@ -11,18 +11,21 @@ read -p "请输入远程端服务器统一ssh端口：" sshPort
 curl -sSL "${mediaUnlockTest}" | grep ^"# \*" | cut -d "*" -f 2 | tr "\n" ","|sed -e 's/,$/\n/'
 
 if ! curl ${proxyUrl} > /dev/null 2>&1; then
-    curl -sSL "${mediaUnlockTest}" | grep ^"# \*" | cut -d "*" -f 2 | tr "\n" ","|sed -e 's/,$/\n/' > output.csv
-    for serverIP in $(cat ip.txt)
-    do
-        ssh -p ${sshPort} root@${serverIP} "bash <(curl -sSL "${mediaUnlockTest}")" | tr "\n" ","|sed -e 's/,$/\n/' >> output.csv       
-    done
+  curl -sSL "${mediaUnlockTest}" | grep ^"# \*" | cut -d "*" -f 2 | tr "\n" ","|sed -e 's/,$/\n/' > output.csv
+  for serverIP in $(cat ip.txt)
+  do
+      ssh -p ${sshPort} root@${serverIP} "bash <(curl -sSL "${mediaUnlockTest}")" | tr "\n" ","|sed -e 's/,$/\n/' >> output.csv
+          if [ $? -ne 0 ]; then
+              echo "ssh -p ${sshPort} root@${serverIP} failed"
+          fi
+  done
 else
-    curl -x ${proxyUrl} -sSL "${mediaUnlockTest}" | grep ^"# \*" | cut -d "*" -f 2 | tr "\n" ","|sed -e 's/,$/\n/' > output.csv
-    for serverIP in $(cat ip.txt)
-    do
-        ssh -p ${sshPort} -o "ProxyCommand=nc --proxy ${proxyUrl} --proxy-type=socks5 %h %p" root@${serverIP} "bash <(curl -sSL "${mediaUnlockTest}")"|tr "\n" ","|sed -e 's/,$/\n/' >> output.csv
-            if [ $? -ne 0 ]; then
-                echo "ssh root@${serverIP} failed"
-            fi
+  curl -x ${proxyUrl} -sSL "${mediaUnlockTest}" | grep ^"# \*" | cut -d "*" -f 2 | tr "\n" ","|sed -e 's/,$/\n/' > output.csv
+  for serverIP in $(cat ip.txt)
+  do
+      ssh -p ${sshPort} -o "ProxyCommand=nc --proxy ${proxyUrl} --proxy-type=socks5 %h %p" root@${serverIP} "bash <(curl -sSL "${mediaUnlockTest}")"|tr "\n" ","|sed -e 's/,$/\n/' >> output.csv
+          if [ $? -ne 0 ]; then
+              echo "ssh -p ${sshPort} root@${serverIP} failed"
+          fi
     done
 fi
